036d6ae0a20940b27a5470136e41a62e360b0f25
Mateusz Wypych <wypychmat@gmail.com>
2021-06-20 19:46:31 +0200

    added missing class

---
+++ client-service/src/main/java/com/wypychmat/clientservice/controller/dto/ClientDto.java
@@ -0,0 +1,26 @@
+package com.wypychmat.clientservice.controller.dto;
+
+public class ClientDto {
+
+    private int id;
+    private String name;
+
+    public ClientDto() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
